f3b96571fa92c9b0b0052f7cc9c17c705e801401
EinsVision <einsvision@gmail.com>
2021-02-21 00:07:31 +0900

    inserted banner image

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -9,6 +9,7 @@ function Home() {
         src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg' 
         alt=''
       />
+      <h1>Home</h1>
     </div>
   )
 }
