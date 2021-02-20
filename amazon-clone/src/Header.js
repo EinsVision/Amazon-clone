52abd5132ccae0ce86ea45a12e4625aff9145ca2
EinsVision <einsvision@gmail.com>
2021-02-20 23:10:02 +0900

    Nav bar building

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css'
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
   return (
@@ -15,9 +17,50 @@ function Header() {
       </Link>
       
       {/* Search box */}
-      <input type='text' className='header__searchInput' />
-
+      <div className='header__search'>
+        <input type='text' className='header__searchInput' />
+        <SearchIcon className='header__searchIcon' />
+      </div>
+      
       {/* 3 Links */}
+      <div className='header__nav'>
+
+        {/* 1st link */}
+        <Link to='/login' className='header__link'>
+          <div className='header__option'>
+            <span className='header__optionLineOne'>Hello Sonny</span>
+            <span className='header__optionLineTwo'>Sign In</span>
+          </div>
+        </Link>
+
+        {/* 2nd link */}
+        <Link to='/' className='header__link'>
+          <div className='header__option'>
+            <span className='header__optionLineOne'>Returns</span>
+            <span className='header__optionLineTwo'>& Orders</span>
+          </div>
+        </Link>
+
+        {/* 3rd link */}
+        <Link to='/' className='header__link'>
+          <div className='header__option'>
+            <span className='header__optionLineOne'>Your</span>
+            <span className='header__optionLineTwo'>Prime</span>
+          </div>
+        </Link>
+
+        {/* 4th link  */}
+        <Link to='checkout' className='header__link'>
+          <div className='header__optionBasket'>
+            {/* Shopping basket icon */}
+            <ShoppingBasketIcon />
+
+            {/* Number of items in the basket */}
+            <span>0</span>
+          </div>
+        </Link>
+      </div>
+
       {/* Basket icon with number */}
     </nav>
   )
