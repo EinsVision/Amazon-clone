446c1ae56a51fc266525528bf3253a887bd2e6c3
EinsVision <einsvision@gmail.com>
2021-02-22 01:00:07 +0900

    login, register built

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -10,8 +10,41 @@ import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
+import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
+
 
 function App() {
+
+  const [{user}, dispatch] = useStateValue();
+
+  // useEffect() ! powerful! 
+  // Piece of code which runs based on a given condition
+
+  useEffect(() =>{
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        // the user is logged in...
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        })
+      } else {
+        // the user is logged out...
+        dispatch({
+          type: 'SET_USER',
+          user: null,
+        })
+      }
+    });
+    return () => {
+      // any cleanup operation go in here...
+      unsubscribe();
+    }
+  }, []);
+
+  console.log("user is -->>", user);
+
   return (  
     <Router>
       <div className="app">
